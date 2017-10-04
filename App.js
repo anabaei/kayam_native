30ce9fcc1bee195ba4ed1399db523a026221994d
Amir Nabaei <amir_nabaei@Amirs-MacBook-Air-5.local>
2017-10-03 19:36:26 -0700

    add kayam mobile app version

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button title="Learn More" onPress={()=> alert('I was passe!')} />
+
+        <Text> Some texts!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
